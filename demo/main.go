833ffae8166e32e09d79163152166c7dbf9fd368
Wall <307606056@qq.com>
2019-12-04 09:30:26 +0800

    消息中间件封装

--- demo/main.go
+++ demo/main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/Wall-js/nebula"
 	hello "github.com/Wall-js/nebula/demo/grpc/service/hello"
 	"github.com/Wall-js/nebula/demo/handler"
-	"github.com/Wall-js/nebula/demo/subscriber"
 	"github.com/micro/go-micro"
 )
 
@@ -13,7 +12,7 @@ func main() {
 		micro.Name("nebula.core.srv.hello"),
 	)
 	hello.RegisterHelloHandler(nebula.Service.Server(), new(handler.Hello))
-	micro.RegisterSubscriber("nebula.core.srv.hello", nebula.Service.Server(), new(subscriber.Hello))
-	micro.RegisterSubscriber("nebula.core.srv.hello", nebula.Service.Server(), subscriber.Handler)
+	//micro.RegisterSubscriber("nebula.core.srv.hello", nebula.Service.Server(), new(subscriber.Hello))
+	//micro.RegisterSubscriber("nebula.core.srv.hello", nebula.Service.Server(), subscriber.Handler)
 	nebula.Run()
 }
